fa4ab58d6a1078771d04f1f985694adad8a60fe1
user <57417394+Divna99@users.noreply.github.com>
2021-04-02 22:16:15 +0200

    Prvi komit
    Pravljenje skeleta aplikacije

---
+++ app/src/main/java/com/example/rollforkotlin/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.rollforkotlin
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.rollforkotlin.ui.main.SectionsPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+
+        val tabs: TabLayout = findViewById(R.id.tabs)
+
+
+
+
+
+    }
+}
