04a70e3ede6a65a6b64382a2c7e0196cac00fefd
user <57417394+Divna99@users.noreply.github.com>
2021-04-06 21:39:09 +0200

    Stilizovan fragment Race

--- app/src/main/java/com/example/rollforkotlin/MainActivity.kt
+++ app/src/main/java/com/example/rollforkotlin/MainActivity.kt
@@ -5,8 +5,7 @@ import com.google.android.material.tabs.TabLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rollforkotlin.ui.main.SectionsPagerAdapter
 import com.example.rollforkotlin.ui.main.adapters.ViewPagerAdapter
-import com.example.rollforkotlin.ui.main.fragments.AbilityFragment
-import com.example.rollforkotlin.ui.main.fragments.RaceFragment
+import com.example.rollforkotlin.ui.main.fragments.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_screen1.*
 
@@ -30,10 +29,10 @@ class MainActivity : AppCompatActivity(){
         //Adding all tab fragments
         adapter.addFragment(RaceFragment(), "")
         adapter.addFragment(AbilityFragment(), "")
-        adapter.addFragment(RaceFragment(), "")
-        adapter.addFragment(AbilityFragment(), "")
-        adapter.addFragment(RaceFragment(), "")
-        adapter.addFragment(AbilityFragment(), "")
+        adapter.addFragment(ToolsFragment(), "")
+        adapter.addFragment(SpellsFragment(), "")
+        adapter.addFragment(EquipmentFragment(), "")
+        adapter.addFragment(DetailsFragment(), "")
 
         viewPager.adapter = adapter
         tabs.setupWithViewPager(viewPager)
@@ -42,7 +41,7 @@ class MainActivity : AppCompatActivity(){
         tabs.getTabAt(0)!!.setIcon(R.drawable.sword3)
         tabs.getTabAt(1)!!.setIcon(R.drawable.stats2)
         tabs.getTabAt(2)!!.setIcon(R.drawable.tools)
-        tabs.getTabAt(3)!!.setIcon(R.drawable.book)
+        tabs.getTabAt(3)!!.setIcon(R.drawable.book1)
         tabs.getTabAt(4)!!.setIcon(R.drawable.bag2)
         tabs.getTabAt(5)!!.setIcon(R.drawable.more)
 
