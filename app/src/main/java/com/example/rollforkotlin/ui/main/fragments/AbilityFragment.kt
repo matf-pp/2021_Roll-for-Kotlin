4ed04934ca61273129e060edd8aca40311a3eaf2
user <57417394+Divna99@users.noreply.github.com>
2021-04-07 11:56:31 +0200

    DOdata funkcionalnost dugmica

--- app/src/main/java/com/example/rollforkotlin/ui/main/fragments/AbilityFragment.kt
+++ app/src/main/java/com/example/rollforkotlin/ui/main/fragments/AbilityFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.rollforkotlin.R
+import kotlinx.android.synthetic.main.fragment_ability.*
+import kotlin.random.Random
 
 
 class AbilityFragment : Fragment() {
@@ -13,11 +15,41 @@ class AbilityFragment : Fragment() {
         super.onCreate(savedInstanceState)
         arguments?.let {
         }
+
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ability, container, false)
+
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        btnStrength.setOnClickListener{
+            val rand = Random.nextInt(1,21)
+            btnStrength.text = rand.toString()
+        }
+        btnCharisma.setOnClickListener{
+            val rand = Random.nextInt(1,21)
+            btnCharisma.text = rand.toString()
+        }
+        btnConstitution.setOnClickListener{
+            val rand = Random.nextInt(1,21)
+            btnConstitution.text = rand.toString()
+        }
+        btnDexterity.setOnClickListener{
+            val rand = Random.nextInt(1,21)
+            btnDexterity.text = rand.toString()
+        }
+        btnIntelligence.setOnClickListener{
+            val rand = Random.nextInt(1,21)
+            btnIntelligence.text = rand.toString()
+        }
+        btnWisdom.setOnClickListener{
+            val rand = Random.nextInt(1,21)
+            btnWisdom.text = rand.toString()
+        }
+    }
+
 }
