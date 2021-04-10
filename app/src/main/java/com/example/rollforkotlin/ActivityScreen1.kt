42bd7f570514f18c23542981a4ebedd1c4d2f756
user <57417394+Divna99@users.noreply.github.com>
2021-04-10 14:39:01 +0200

    Uzimanje podataka iz objekata

--- app/src/main/java/com/example/rollforkotlin/ActivityScreen1.kt
+++ app/src/main/java/com/example/rollforkotlin/ActivityScreen1.kt
@@ -1,8 +1,12 @@
 package com.example.rollforkotlin
 
 import android.content.Intent
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.RadioButton
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_screen1.*
 
 class ActivityScreen1 : AppCompatActivity() {
@@ -10,8 +14,41 @@ class ActivityScreen1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen1)
         continueButton.setOnClickListener{
+            getValues()
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
         }
+
+        // Create an ArrayAdapter
+        val adapter = ArrayAdapter.createFromResource(this,
+                R.array.levels, R.layout.spinner_item)
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        //R.style.SpinnerTheme
+        // Apply the adapter to the spinner
+        spLevel.adapter = adapter
+
+
     }
+
+
+    fun getValues() {
+        val level = spLevel.selectedItem.toString().substring(0,1)
+        var gender : String = ""
+        val selectedRadioButtonId: Int = radioGroup.checkedRadioButtonId
+        if (selectedRadioButtonId != -1) {
+            val  selectedRadioButton : RadioButton  = findViewById(selectedRadioButtonId)
+            gender = selectedRadioButton.text.toString()
+        }
+        val name = txtName.text
+
+       /* val builder = AlertDialog.Builder(this)
+        builder.setTitle("Androidly Alert")
+        builder.setMessage(level)
+        builder.show()*/
+
+    }
+
 }
+
+
